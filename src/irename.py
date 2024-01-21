ab6a4306b1218f255d22560611814556f01ccd6d
gaofengg <ymmtd@163.com>
2024-01-22 03:45:34 +0800

    add comments

--- src/irename.py
+++ src/irename.py
@@ -1,4 +1,5 @@
 """_summary_
+
     Introductions:
         1. 用于批量重命名文件名
 """
@@ -6,11 +7,15 @@ import os
 
 DIRECTORY_PATH = "./"
 file_names = os.listdir(DIRECTORY_PATH)
+# 通过特征查找文件
+# 如果文件名中，从第一个字符到第一个 '-' 字符之间的字符串长度小于 3，则将其保留到一个新的列表中
 result: list = [n for n in file_names
                 if n.find("-") != -1 if len(n.split("-")[0]) < 3]
+# 遍历列表
 for n in result:
     length: int = len(n.split("-")[0])
     delta_len = 3 - length
+    # 需要部几个零到文件名首位
     add_zero: str = "0" * delta_len
     new_name: str = add_zero + n
     os.rename(n, new_name)
