bcb55ba2bdf659db8bd6c707efb308f08ffac487
gaofengg <ymmtd@163.com>
2024-01-25 23:54:13 +0800

    fix bug: when result is null

--- src/irename.py
+++ src/irename.py
@@ -12,11 +12,12 @@ file_names = os.listdir(DIRECTORY_PATH)
 result: list = [n for n in file_names
                 if n.find("-") != -1 if len(n.split("-")[0]) < 3]
 # 遍历列表
-for n in result:
-    length: int = len(n.split("-")[0])
-    delta_len = 3 - length
-    # 需要部几个零到文件名首位
-    add_zero: str = "0" * delta_len
-    new_name: str = add_zero + n
-    os.rename(n, new_name)
-    print(f"文件名 {n} 成功被修改为 {new_name}。")
+if result:
+    for n in result:
+        length: int = len(n.split("-")[0])
+        delta_len = 3 - length
+        # 需要部几个零到文件名首位
+        add_zero: str = "0" * delta_len
+        new_name: str = add_zero + n
+        os.rename(n, new_name)
+        print(f"文件名 {n} 成功被修改为 {new_name}。")
